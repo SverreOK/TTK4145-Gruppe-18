4d6a8df4282edaf3e3ab4d6c68ce2b51b980406b
SverreOK <sverre.o.k@hotmail.no>
2024-01-17 14:42:43 +0100

    Exercise 1, task 3

--- exercises/exercise_1/shared variable/c/foo.c
+++ exercises/exercise_1/shared variable/c/foo.c
@@ -9,11 +9,17 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    for (int x = 0; x < 1000000; x++) {
+        i++;
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+    for (int m = 0; m < 1000000; m++) {
+        i--;
+    }
     return NULL;
 }
 
@@ -22,10 +28,17 @@ int main(){
     // TODO: 
     // start the two functions as their own threads using `pthread_create`
     // Hint: search the web! Maybe try "pthread_create example"?
+
+    pthread_t thread_increment, thread_decrement;
+
+    pthread_create(&thread_increment, NULL, incrementingThreadFunction, NULL);
+    pthread_create(&thread_decrement, NULL, decrementingThreadFunction, NULL);
     
     // TODO:
     // wait for the two threads to be done before printing the final result
-    // Hint: Use `pthread_join`    
+    // Hint: Use `pthread_join` 
+    pthread_join(thread_increment, NULL);
+    pthread_join(thread_decrement, NULL);
     
     printf("The magic number is: %d\n", i);
     return 0;
---
+++ exercises/exercise_1/shared variable/results.md
@@ -0,0 +1,5 @@
+# Results for task 3:
+- GO gave 662380 on first run and 842572 on second run
+- C gave 328275 on first run and 344917 on second run
+
+- Both codes have a race condition, where both threads race to either increment or decrement the variable i. 
