90fbbe0930ca35393827941510ea42e915eb2fde
SverreOK <sverre.o.k@hotmail.no>
2024-01-17 14:58:35 +0100

    Task 5, go

--- exercises/exercise_1/bounded buffer/go/main.go
+++ exercises/exercise_1/bounded buffer/go/main.go
@@ -4,35 +4,42 @@ package main
 import "fmt"
 import "time"
 
+const buffer_size = 5;
 
-func producer(/*TODO: parameters?*/){
+func producer(buffer chan<- int){
 
     for i := 0; i < 10; i++ {
         time.Sleep(100 * time.Millisecond)
         fmt.Printf("[producer]: pushing %d\n", i)
         // TODO: push real value to buffer
+        buffer <- i
     }
 
 }
 
-func consumer(/*TODO: parameters?*/){
+func consumer(buffer <-chan int){
 
     time.Sleep(1 * time.Second)
     for {
-        i := 0 //TODO: get real value from buffer
+        i := <-buffer  //TODO: get real value from buffer
         fmt.Printf("[consumer]: %d\n", i)
         time.Sleep(50 * time.Millisecond)
     }
     
 }
 
-
 func main(){
     
     // TODO: make a bounded buffer
+    /* 
+        Buffered channel as in the Go tutorial.
+        If buffered channel is full and producer tries to push, the producer will wait until the consumer has "consumed".
+    */
+    buffer := make(chan int, buffer_size);
     
-    go consumer()
-    go producer()
+    go consumer(buffer)
+    go producer(buffer)
     
+    // Select causes problem when no other routine runs?
     select {}
 }
