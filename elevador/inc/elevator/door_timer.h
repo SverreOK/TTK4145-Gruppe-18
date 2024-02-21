e955bc04cb2be25d29142fd5d29149889aa4e225
SverreOK <sverre.o.k@hotmail.no>
2024-02-21 21:07:52 +0100

    fixed a bit on door_timer

--- elevador/inc/elevator/door_timer.h
+++ elevador/inc/elevator/door_timer.h
@@ -2,6 +2,7 @@
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <functional>
+#include "elevator/elevator_fsm.h"
 
 #define RESET_TIME 3
 
@@ -10,8 +11,8 @@ public:
     door_timer(boost::asio::io_service& io);
     ~door_timer();
 
-    void start(std::function<void()> timeoutCallback);
-    void reset();
+    void start(std::function<void(elevator_event)> timeout_callback);
+    void reset(std::function<void(elevator_event)> timeout_callback);
     bool isRunning();
 
 private:
--- elevador/src/elevator/door_timer.cpp
+++ elevador/src/elevator/door_timer.cpp
@@ -6,20 +6,27 @@ door_timer::~door_timer() {
     timer.cancel();
 }
 
-void door_timer::start(std::function<void()> timeoutCallback) {
+void door_timer::start(std::function<void(elevator_event)> timeout_callback) {
     running = true;
     timer.expires_from_now(boost::posix_time::seconds(RESET_TIME));
-    timer.async_wait([this, timeoutCallback](const boost::system::error_code& error) {
+    timer.async_wait([this, timeout_callback](const boost::system::error_code& error) {
         if (!error && running) {
             running = false;
-            timeoutCallback();
+            timeout_callback(elevator_event::DOOR_TIMEOUT);
         }
     });
 }
 
-void door_timer::reset() {
-    running = true;
+void door_timer::reset(std::function<void(elevator_event)> timeout_callback) {
+    timer.cancel(); // Cancel the existing async wait
     timer.expires_from_now(boost::posix_time::seconds(RESET_TIME));
+    running = true;
+    timer.async_wait([this, timeout_callback](const boost::system::error_code& error) {
+        if (!error && running) {
+            running = false;
+            timeout_callback(elevator_event::DOOR_TIMEOUT);
+        }
+    });
 }
 
 bool door_timer::isRunning() {
