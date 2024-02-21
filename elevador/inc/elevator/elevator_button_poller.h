50f1e0c2b376626258ba41eae53b63d40aa9f606
endurs <smilybobgaming@gmail.com>
2024-02-21 13:52:49 +0100

    moved some files

---
+++ elevador/inc/elevator/elevator_button_poller.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "call_class.h"
+#include "elevator_driver.h"
+#include <boost/thread.hpp>
+
+class button_poller {
+private:
+    elevator_driver* driver;
+    Call_database* call_database;
+    boost::thread poller_thread;
+    bool running;
+    int number_of_floors;
+
+    void poll_buttons() {
+        while (running) {
+            for (int floor = 0; floor < number_of_floors; ++floor) {
+                for (int button = 0; button < 3; ++button) {
+                    if (driver->get_button_signal(button, floor) == 1) {
+
+                        Call new_call(floor, static_cast<button_type>(button), 0); //TODO ADD REAL CALL ID
+
+                        call_database->add_call(); //TODO ADD REAL CALL FUNC
+                    }
+                }
+            }
+        }
+    }
+
+public:
+    button_poller(elevator_driver* driver) : driver(driver), running(false) {}
+
+    void start() {
+        running = true;
+        poller_thread = boost::thread(&button_poller::poll_buttons, this);
+    }
+
+    void stop() {
+        running = false;
+        if (poller_thread.joinable()) {
+            poller_thread.join();
+        }
+    }
+
+    ~button_poller() {
+        stop();
+    }
+};
