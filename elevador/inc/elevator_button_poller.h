111ff8a822a94bf27125263a20e2f4a26f52aaed
endurs <smilybobgaming@gmail.com>
2024-02-21 13:40:22 +0100

    .

--- elevador/inc/elevator_button_poller.h
+++ elevador/inc/elevator_button_poller.h
@@ -6,16 +6,16 @@
 
 class button_poller {
 private:
-    elevator_driver* elevator_driver_;
+    elevator_driver* elevator_driver;
     boost::thread poller_thread_;
     bool running_;
 
     void poll_buttons() {
         while (running_) {
-            for (int floor = 0; floor < elevator_driver_.get_number_of_floors(); ++floor) {
+            for (int floor = 0; floor < elevator_driver->get_number_of_floors(); ++floor) {
                 for (int button = 0; button < 3; ++button) {
-                    if (elevator_driver_.get_button_state(floor, button) == 1) {
-                        elevator_driver_.create_call(floor, button);
+                    if (elevator_driver->get_button_state(floor, button) == 1) {
+                        elevator_driver->create_call(floor, button);
                     }
                 }
             }
