e323aaa2c3e6de889ecd119db8437c3fb7342b25
SverreOK <sverre.o.k@hotmail.no>
2024-03-04 01:54:09 +0100

    updated config :grinning:

--- elevador/inc/config.h
+++ elevador/inc/config.h
@@ -1,1 +1,8 @@
 #define NUM_ELEVATORS 3 //max, not current
+#define NUM_FLOORSS 4 //max, not current  // I had double s because it might be used, idk, change when going through the code
+
+#define FSM_RATE 100 
+#define POLLER_RATE 100 
+#define LIGHT_RATE 100
+
+#define DEBUG 0 // 1 for debug, 0 for no debug --> implement a command line based GUI?
