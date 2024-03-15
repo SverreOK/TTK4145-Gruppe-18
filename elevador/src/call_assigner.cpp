3677cbd1a36c55773fc2351c06b073c8e7522b9c
Martin Salthe <martin.salthe@propulsentnu.no>
2024-03-15 19:19:21 +0100

    :grinning:

--- elevador/src/call_assigner.cpp
+++ elevador/src/call_assigner.cpp
@@ -23,7 +23,7 @@ static std::vector<std::vector<bool>> call_list_to_floor_list(std::vector<Call*>
 }
 
 // Execute a command and return the output as a string
-static std::string exec(const char* cmd) { 
+std::string exec(const char* cmd) { 
     std::array<char, 128> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
