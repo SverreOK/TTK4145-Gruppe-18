35e1047ce196fc08dbb17fe02bc72b6be8fb0210
SverreOK <sverre.o.k@hotmail.no>
2024-03-15 21:36:59 +0100

    small bug :grinning:

--- elevador/inc/startup/process_pair.h
+++ elevador/inc/startup/process_pair.h
@@ -14,65 +14,6 @@ using namespace std::chrono_literals;
 
 auto last_receive_time = std::chrono::steady_clock::now();
 
-// Forward declaration
-void run_primary(const std::string& host, const std::string& port); 
-
-// Runs the backup
-// Checks for heartbeat from the primary thread
-// If no heartbeats are found, it will begin as a primary thread and launch backup window
-void run_backup(const std::string& host, const std::string& port) {
-    try {
-        
-        // SOCKETS FOR BACKUP
-        boost::asio::io_service io_service;
-        udp::socket socket(io_service, udp::endpoint(udp::v4(), std::stoi(port)));
-        socket.set_option(udp::socket::reuse_address(true));
-        socket.non_blocking(true);
-        udp::endpoint sender_endpoint;
-
-        bool is_primary_now = false;
-        char buf[1024];
-
-        while (!is_primary_now) {
-            std::cout << "Backup running" << std::endl;
-
-            boost::system::error_code error;
-            size_t length = socket.receive_from(boost::asio::buffer(buf), sender_endpoint, 0, error);
-
-            // Checks if the primary is still alive, if not 
-            if (error == boost::asio::error::would_block) {
-            
-                if (!is_primary_now && std::chrono::steady_clock::now() - last_receive_time > 3s) {
-                    is_primary_now = true;
-                    run_primary(host, PRIMARY_PORT);
-
-                    break;
-                }
-
-                // sleep for 1 second
-                boost::this_thread::sleep_for(boost::chrono::milliseconds(HEARTBEAT_RATE_MS));
-                continue;
-            }
-
-            if (error && error != boost::asio::error::message_size) {
-                    throw boost::system::system_error(error);
-            }
-
-            // Reset last_receive time
-            last_receive_time = std::chrono::steady_clock::now();
-        }
-
-        socket.close();
-
-        // Run the primary because you are now the primary :))
-        run_primary(host, PRIMARY_PORT);
-    } 
-
-    catch (std::exception& e) {
-        std::cerr << "Backup Exception: " << e.what() << std::endl;
-    }
-}
-
 // Runs the primary
 // Sends heartbeats to the backup
 // Runs the primary dispatch logic
@@ -105,5 +46,60 @@ void run_primary(const std::string& host, const std::string& port) {
     } 
     catch (std::exception& e) {
         std::cerr << "Primary Exception: " << e.what() << std::endl;
+    }
+}
+
+// Runs the backup
+// Checks for heartbeat from the primary thread
+// If no heartbeats are found, it will begin as a primary thread and launch backup window
+void run_backup(const std::string& host, const std::string& port) {
+    try {
+        
+        // SOCKETS FOR BACKUP
+        boost::asio::io_service io_service;
+        udp::socket socket(io_service, udp::endpoint(udp::v4(), std::stoi(port)));
+        socket.set_option(udp::socket::reuse_address(true));
+        socket.non_blocking(true);
+        udp::endpoint sender_endpoint;
+
+        bool is_primary_now = false;
+        char buf[1024];
+
+        while (!is_primary_now) {
+            std::cout << "Backup running" << std::endl;
+
+            boost::system::error_code error;
+            size_t length = socket.receive_from(boost::asio::buffer(buf), sender_endpoint, 0, error);
+
+            // Checks if the primary is still alive, if not 
+            if (error == boost::asio::error::would_block) {
+            
+                if (!is_primary_now && std::chrono::steady_clock::now() - last_receive_time > 3s) {
+                    is_primary_now = true;
+
+                    break;
+                }
+
+                // sleep for 1 second
+                boost::this_thread::sleep_for(boost::chrono::milliseconds(HEARTBEAT_RATE_MS));
+                continue;
+            }
+
+            if (error && error != boost::asio::error::message_size) {
+                    throw boost::system::system_error(error);
+            }
+
+            // Reset last_receive time
+            last_receive_time = std::chrono::steady_clock::now();
+        }
+
+        socket.close();
+
+        // Run the primary because you are now the primary :))
+        run_primary(host, PRIMARY_PORT);
+    } 
+
+    catch (std::exception& e) {
+        std::cerr << "Backup Exception: " << e.what() << std::endl;
     }
 }
