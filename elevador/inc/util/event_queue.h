0d8d13b6cf9f007dd26cbb6331ee8e20af909095
andersBlanders <smilybobgaming@gmail.com>
2024-03-17 23:21:57 +0100

    event_queue

--- elevador/inc/util/event_queue.h
+++ elevador/inc/util/event_queue.h
@@ -32,56 +32,11 @@ class thread_safe_queue {
         elevator_event last_popped_event;
     
     public: 
-        // Pushes an element to the queue 
-        void push(elevator_event item) 
-        { 
-            // Acquire lock 
-            std::unique_lock<std::mutex> lock(m_mutex); 
-    
-            // Add item 
-            m_queue.push(item); 
-    
-            // Notify one thread that 
-            // is waiting 
-            m_cond.notify_one(); 
-        } 
-    
-        // Pops an element off the queue 
-        /*
-        To use, 
-        Elevator::handle_event(event_queue.pop());    
-        */
-        elevator_event pop() 
-        { 
-    
-            // acquire lock 
-            std::unique_lock<std::mutex> lock(m_mutex); 
-    
-            // wait until queue is not empty 
-            m_cond.wait(lock, 
-                        [this]() { return !m_queue.empty(); }); 
-    
-            // retrieve item 
-            elevator_event item = m_queue.front();
-            m_queue.pop(); 
-            last_popped_event = item;
-    
-            // return item
-            return item; 
-        }
+        void push(elevator_event item);
+        elevator_event pop();
 
-        bool empty() {
-            std::unique_lock<std::mutex> lock(m_mutex);
-            return m_queue.empty();
-        }
+        bool empty();
 
-        elevator_event front() {
-            std::unique_lock<std::mutex> lock(m_mutex);
-            return m_queue.front();
-        }
-
-        elevator_event get_last_popped_event() {
-            std::lock_guard<std::mutex> lock(m_mutex);
-            return last_popped_event;
-        }
+        elevator_event front();
+        elevator_event get_last_popped_event();
 }; 
