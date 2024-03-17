673d96b3ba2a9b8c220553a3315b362e6f358578
Martin Salthe <martin.salthe@propulsentnu.no>
2024-03-17 19:47:12 +0100

    Cleaned up elevator_state_class.h

--- elevador/inc/data/elevator_state_class.h
+++ elevador/inc/data/elevator_state_class.h
@@ -5,11 +5,12 @@
 #include <boost/thread/mutex.hpp>
 
 
-struct elevator_status_network
-{
+struct elevator_status_network {
     char id[8];
+
     uint8_t state;
     int8_t floor;
+
     bool obstruction;
     bool alive;
 };
@@ -18,14 +19,18 @@ class Elevator_state {
     private:
         Elevator_id id;
         state_enum current_state;
+
         int current_floor;
-        bool obstruction;
-        bool alive;
-        boost::asio::ip::udp::endpoint endpoint;
-        boost::mutex mtx;
         int last_seen;
 
+        bool obstruction;
+        bool alive;
+
+        boost::asio::ip::udp::endpoint endpoint;
+        boost::mutex mtx;
+
     public:
+        // Constructors
         Elevator_state(Elevator_id id) : id(id){
             this->current_state = state_enum::IDLE;
             this->current_floor = 0;
@@ -33,6 +38,7 @@ class Elevator_state {
             this->alive = true;
             this->last_seen = time(NULL);
         }
+
         Elevator_state(elevator_status_network status) : id(Elevator_id {status.id}){
             this->current_state = static_cast<state_enum>(status.state);
             this->current_floor = status.floor;
@@ -41,24 +47,25 @@ class Elevator_state {
             this->last_seen = time(NULL);
         }
 
-        state_enum get_current_state();
-        int get_current_floor();
+        // Getters        
         bool get_obstruction_status();
         bool get_alive_status();
+
+        int get_current_floor();
+        int get_last_seen();
+
+        state_enum get_current_state();
         elevator_status_network get_status_network();
         Elevator_id get_id();
-        int get_last_seen();
-        
 
+        // Needed for json creation
+        std::string get_behaviour();
+        std::string get_direction();
+
+        // Setters
         void set_current_state(state_enum state);
         void set_current_floor(int floor);
         void set_obstruction(bool obstruction);
         void set_alive(bool alive);
         void set_last_seen(void);
-
-
-        // Needed for json creation:
-        std::string get_behaviour();
-        std::string get_direction();
-        std::vector<bool> get_cab_requests();
 };
